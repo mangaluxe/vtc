88b7c9f4a0773e7cadc2a515d8c4eae3c95daab3
Unknown <furyfatal@laposte.net>
2021-05-24 16:48:10 +0200

    up

--- inc/db.php
+++ inc/db.php
@@ -1,15 +1,12 @@
 <?php
-try
-{
+// Connexion à la BDD via PDO ! Try... catch pour tester
+try {
 	$db = new PDO('mysql:host=localhost; dbname=vtc; charset=utf8', 'root', '', [
-		PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING, // Active les erreurs SQL,
-		// On récupère tous les résultats en tableau associatif
-		PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+		PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING, /* Active les erreurs SQL */
+		PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC /* On récupère tous les résultats en tableau associatif */
 	]);
 }
-catch(Exception $e)
-{
-    echo 'Erreur de connexion à la base de données.';
-    // echo $e->getMessage(); // Affiche message d'erreur si la connexion échoue
+catch(Exception $e) {
+    echo 'Erreur de connexion à la base de données : '.$e->getMessage(); // Affiche message d'erreur si la connexion échoue
 }
 ?>
